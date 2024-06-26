88e81594f09daa270014e9a6bd258ed9138c6490
A032003 <130210747+A032003@users.noreply.github.com>
2024-06-26 09:36:36 +0530

    Add files via upload

---
+++ Balance a Binary Search Tree-26_June_2024.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& vec) {
+        if (root == NULL) {
+            return;
+        }
+        inorder(root->left, vec);
+        vec.push_back(root->val);
+        inorder(root->right, vec);
+    }
+
+    TreeNode* construct(vector<int> vec, int l, int r) {
+        if (l > r) {
+            return NULL;
+        }
+
+        int mid = l + (r - l) / 2;
+        TreeNode* root = new TreeNode(vec[mid]);
+        root->left = construct(vec, l, mid - 1);
+        root->right = construct(vec, mid + 1, r);
+        return root;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> vec;
+        inorder(root, vec);
+        int l = 0;
+        int r = vec.size() - 1;
+
+        return construct(vec, l, r);
+    }
+};
