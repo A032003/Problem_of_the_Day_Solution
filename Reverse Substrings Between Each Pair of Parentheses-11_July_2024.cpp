ca3341f2fa55e8b677cc5a6d8b47a2a357bfb5df
A032003 <130210747+A032003@users.noreply.github.com>
2024-07-11 11:15:11 +0530

    Add files via upload

---
+++ Reverse Substrings Between Each Pair of Parentheses-11_July_2024.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> st;
+
+        string result;
+
+        for (char& ch : s) {
+            if (ch == '(') {
+                st.push(result.length());
+            } else if (ch == ')') {
+                int l = st.top();
+                st.pop();
+                reverse(begin(result) + l, end(result));
+            } else {
+                result.push_back(ch);
+            }
+        }
+
+        return result;
+    }
+};
